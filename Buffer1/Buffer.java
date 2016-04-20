9c5b20e6a64bbd8572b6267eb432fa3281908beb
azthec <azthec@users.noreply.github.com>
2016-04-20 18:32:29 +0100

    Update Buffer.java

--- Buffer1/Buffer.java
+++ Buffer1/Buffer.java
@@ -1,118 +1,125 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
-import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-public class FileBuffer extends Buffer {
-    private Path savePath;
-    // null= não definido
-    private boolean modified;
-    // true= modificado; false= inalterado
+/**
+ * Created by up201103891 on 4/19/16.
+ */
 
+public class Buffer {
+    protected List<StringBuilder> lineList; //lineList.get(3) obtem o quarto elemento da lista
+    private int cursorRow, cursorCol;
 
-    FileBuffer() { //para ter construtor
-        super();
+    // inicialização
+    public Buffer() {
+        lineList = new ArrayList<StringBuilder>();
+        lineList.add(new StringBuilder(""));
+        cursorRow = 0;
+        cursorCol = 0;
     }
 
-    // gravar
-    public void save()
-            throws IOException {
-        File file = new File(savePath.toString());
-
-        if(modified) {
-            if(!file.exists()){
-                try {
-                    file.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            PrintWriter writer = null;
-            try {
-                writer = new PrintWriter(savePath.toString(), "UTF-8");
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-
-            for(StringBuilder string : lineList) {
-                writer.println(string.toString());
-            }
-            writer.close();
-
-            modified = false;
-        }
-
-    }
-
-    public void saveAs(Path path)
-            throws IOException {
-        savePath = path;
-        save();
-    }
-
-    // abrir
-    public void open(Path path)
-            throws IOException {
-        savePath = path;
-        File file = new File(savePath.toString());
-
-        if(!file.exists()){
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-
-            String line;
-            try (
-                    InputStream fis = new FileInputStream(file);
-                    InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
-                    BufferedReader br = new BufferedReader(isr);
-            ) {
-                while ((line = br.readLine()) != null) {
-                    line = line.trim();
-                    insertString(line);
-                    breakLine();
-                }
-                br.close(); //loses the stream according to javadoc for BufferedReader and InputStreamReader as well as FileReader
-            }
-        }
-        savePath = file.toPath();
-    }
-
-    @Override
+    // inserir carater
     public void insert(char c) {
-        super.insert(c);
-        modified = true; // marcar modificação
+    	if(c=='\n') {
+    		breakLine();
+    	} else { //talvez seja preciso adiciona trycatch
+    		insertString(Character.toString(c));
+    	}
     }
-    // análogo para outros modificadores
-
-    @Override
+    //inserior string
     public void insertString(String tmp) {
-        //throws InvalidContentException {
-        //try {
-        super.insertString(tmp);
-        //} catch (InvalidContentException e) {
-        //  e.printStackTrace();
-        //}
-        modified=true;
+    	lineList.get(cursorRow).insert(cursorCol, tmp);
+		cursorCol += tmp.length();
+    }
+    // apagar carater
+
+    public void delete() {
+        if(cursorCol == 0 && cursorRow != 0) {
+            lineList.get(cursorRow-1).append(lineList.get(cursorRow));
+            lineList.remove(cursorRow);
+            cursorCol=lineList.get(cursorRow-1).length();
+            moveUp();
+        } else if(cursorCol != 0) {
+        	//apaga caracter a esquerda do cursor
+            lineList.get(cursorRow).deleteCharAt(cursorCol-1);
+            moveLeft();
+        }
+    }
+    // mover o cursor
+    public void moveUp() {
+    	//se a linha de cima for menor, salta para a posicao do final dela
+    	if(cursorRow > 0)
+    		cursorCol = Math.min(cursorCol, lineList.get(cursorRow-1).length());
     }
 
-    @Override
+    public void moveRight() {
+    	if(cursorCol == lineList.get(cursorRow).length() && cursorRow != lineList.size()-1) {
+    		cursorCol=0;
+    		cursorRow++;
+    	} else if(cursorCol != lineList.get(cursorRow).length()) {
+    		cursorCol++;
+    	}
+    }
+
+    public void moveDown() {
+    	if(cursorRow < lineList.size() -1) {
+    		cursorCol = Math.min(cursorCol, lineList.get(cursorRow+1).length());
+    	}
+    }
+
+    public void moveLeft() {
+    	if(cursorCol == 0 && cursorRow != 0) {
+    		cursorCol=lineList.get(cursorRow-1).length();
+    		cursorRow--;
+    	} else if (cursorCol!=0) {
+    		cursorCol--;
+    	}
+    }
+
+    public int getNLines() {
+    	//ter atencao que as linha comecam no 0
+        return lineList.size();
+    }
+    
     public void breakLine() {
-        super.breakLine();
-        modified = true;
+    	//guardar string da pos cursor ate ao fim da linha
+    	String rest = lineList.get(cursorRow).substring(cursorCol,lineList.get(cursorRow).length());
+    	//apagar stringbuilder do cursor ate ao fim da linha
+    	lineList.get(cursorRow).replace(cursorCol, lineList.get(cursorRow).length(), "");
+    	//adicionar linha nova com string rest
+    	lineList.add(cursorRow+1,new StringBuilder(rest));
+    	//posicionar cursor no novo sitio
+    	cursorRow++;
+    	cursorCol=0;
     }
 
+    public StringBuilder getLine(int nLine) { //ou toString se quisermos retornar strings simples
+        //ter atencao que as linha comecam no 0
+    	return lineList.get(nLine);
+    }
+    
+    public int getcursorCol() {
+    	return cursorCol;
+    }
+    
+    public int getcursorRow() {
+    	return cursorRow;
+    }
+    
+    public boolean validPosition(int row, int col) {
+    	if(0 <= col && col <= lineList.get(row).length())
+    		if(0 <= row && row <= lineList.size()-1)
+    			return true;
+    	return false;
+    }
+    
+    public void setCursor(int row, int col) throws IOException {
+    	if(validPosition(row,col)) {
+    		cursorRow = row;
+    		cursorCol = col;
+    	} else {
+    		throw new IOException();
+    	}
+    }
 }
