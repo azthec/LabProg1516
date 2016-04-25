133fa2243329fe398a7812ea31f98bc62874e80a
azthec <azthec@users.noreply.github.com>
2016-04-25 18:35:41 +0100

    Create CustomExceptions.java

---
+++ Buffer1/CustomExceptions.java
@@ -0,0 +1,28 @@
+/**
+ * Caso posicao do cursor seja invalida
+ *
+ */
+class InvalidCursorPosition extends Exception
+{
+	//http://stackoverflow.com/questions/285793/what-is-a-serialversionuid-and-why-should-i-use-it
+	private static final long serialVersionUID = 1L;
+	
+      //Parameterless Constructor
+      public InvalidCursorPosition() {}
+
+      //Constructor that accepts a message
+ }
+
+/**
+ * Caso posicao da linha pedida nao exista
+ *
+ */
+class LineInputException extends Exception 
+{
+		private static final long serialVersionUID = 1L;
+		
+	      //Parameterless Constructor
+	      public LineInputException() {}
+
+	      //Constructor that accepts a message
+}
